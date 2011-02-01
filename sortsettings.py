60a1c5fcd38c7f177c3cc85fcc23d3367fbcc5a2
Lars van de Kerkhof <lars@permanentmarkers.nl>
2011-02-01 15:17:29 +0100

    fixes

--- sortsettings.py
+++ sortsettings.py
@@ -26,28 +26,27 @@ def main():
         found = False
         
         with open(re.sub(r'pyc|pyo', 'py', settings_module.__file__)) as settings_file:
+            code_buffer = ""
             for line in settings_file:
                 # when a new settings block is found, reset buffer.
                 if SETTING_PATTERN_LOOSE.match(line):
                     active = False
-                    code_buffer = ""
             
                 # if the key is found, activate output of settings block
                 if line.find(key) == 0:
                     active = True
-                    found = True
             
                 # output block but skip empty lines and comments
-                if active and not re.match(r'$\S*^', line) and line[0] != '#':
-                    code_buffer += line[-1]
-                    print line[:-1]
+                if active and not re.match(r'$\s*^', line) and line[0] != '#':
+                    found = True
+                    code_buffer += line
+                    # print line[:-1]
 
                     context = dict()
 
                     # check if the current outputted block is complete by using
                     # eval and comparison with the value from the module
                     try:
-                        code_buffer = line
                         exec code_buffer in settings_module.__dict__, context
                     except Exception as e:
                         pass
@@ -59,6 +58,9 @@ def main():
         if (not group or group not in key) and found:
             group = key[0:key.find('_')]
             print ""
+        
+        if found and code_buffer:
+            print code_buffer[:-1]
         
 if __name__ == "__main__":
     sys.exit(main())
