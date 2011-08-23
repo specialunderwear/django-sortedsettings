6ef23a794e92e5d3c8ec3d585ee6e37b60efab89
Lars van de Kerkhof <lars@permanentmarkers.nl>
2011-08-23 12:53:20 +0200

    added long description

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
+"""
+Django-sortedsettings
+-------------------
 
+A script that outputs all the values in your settings.py, but ordered
+alphabetically.
+
+usage::
+
+    sortsettings.py
+
+This will dump the settings.py module sorted
+
+or::
+
+    sortsettings.py settings.someothermodule
+
+This will dump the settings/someothermodule.py module sorted
+"""
 from distutils.core import setup
 
 setup(
     name='django-sortedsettings',
     version='1.0',
     description="Sort the django settings module's entries alphabetically",
+    long_description=__doc__,
     author='Lars van de Kerkhof',
     author_email='sortedsettings@permanentmarkers.nl',
     url='https://github.com/specialunderwear/django-sortedsettings',
